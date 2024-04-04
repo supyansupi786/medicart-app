75ee10ccb9bdf3a0af371949807d76c2ddda0027
supyansupi786 <156179139+supyansupi786@users.noreply.github.com>
2024-04-04 16:48:56 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import colors from "colors";
-const ConnectURI = "mongodb://localhost:27017/medicine";  
+// const ConnectURI = "mongodb://localhost:27017/medicine";  
 // const ConnectURI = mongodb+srv://supyan:7338091921@ecommerce.07f8bot.mongodb.net/ecommerce;  
 const connectDB = async () => {
   try {
-    await mongoose.connect(ConnectURI);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
       `Conneted To Mongodb Databse `.bgMagenta.white
     );
@@ -12,5 +12,6 @@ const connectDB = async () => {
     console.log(`Errro in Mongodb ${error}`.bgRed.white);
   }
 };
+
 
  export default connectDB;
