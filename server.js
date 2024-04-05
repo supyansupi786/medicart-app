30bef348705d63fc5e0f26cb41d3760cdd2be21a
supyansupi786 <156179139+supyansupi786@users.noreply.github.com>
2024-04-05 10:05:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+{
+origin: ["https://deploy-mern-1whq.vercel.app"],
+methods: ["POST", "GET"],
+credentials: true
+
+}
+ ));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, './client/build')));
